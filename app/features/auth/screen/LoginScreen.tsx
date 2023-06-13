f56227ff88163e4b23e7a71495cb97a9ecc14f88
Md. Nazim Hasan <nazimhasan124073@gmail.com>
2023-06-13 20:38:18 +0600

    (feat fonts): futura and inter font added

--- app/features/auth/screen/LoginScreen.tsx
+++ app/features/auth/screen/LoginScreen.tsx
@@ -4,11 +4,32 @@ import React from 'react'
 const LoginScreen = () => {
   return (
     <View>
-      <Text>LoginScreen</Text>
+      <Text style={styles.text}>LoginScreen</Text>
+      <Text style={styles.text2}>LoginScreen</Text>
+      <Text style={styles.text3}>LoginScreen</Text>
     </View>
   )
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  text: {
+    textAlign: 'center',
+    color: 'black',
+    fontSize: 60,
+    fontFamily: 'Futura Bold Condensed BT'
+  },
+  text2: {
+    textAlign: 'center',
+    color: 'black',
+    fontSize: 60,
+    fontFamily: 'Inter Regular'
+  },
+  text3: {
+    textAlign: 'center',
+    color: 'black',
+    fontSize: 60,
+    fontFamily: 'Inter SemiBold'
+  }
+})
